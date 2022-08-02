b63462a6b8795cefcf4003339a8a0868fd18cc3e
GeneralTim33 <110459244+GeneralTim33@users.noreply.github.com>
2022-08-02 15:06:51 +0200

    Update Token.sol

--- Token.sol
+++ Token.sol
@@ -10,6 +10,33 @@
    }
 }
 
+contract Whitlist {
+address owner; // varible that will contain the address of the cont
+constructor()  {
+    owner = msg.sender; // setting the owner the contract deployer
+    }
+
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Ownable:  caller is not the owner" );
+        _;
+}
+mapping (address => bool) whitelistedAddresses;          
+function addUser(address _addressToWhitelist) public onlyOwner {
+    whitelistedAddresses[_addressToWhitelist] = true;
+}
+function verifyUser(address _whitelistedAddress) public view returns(bool) {
+    bool userIsWhitelisted = whitelistedAddresses[_whitelistedAddress];
+    return userIsWhitelisted;
+}
+modifier isWhitelisted(address _address) {
+    require(whitelistedAddresses[_address], "You need to be whitelisted");_;
+  }
+  
+  function exampleFunction() public view isWhitelisted(msg.sender) returns(bool){
+     return (true);
+  }
+ }
 
 
 
